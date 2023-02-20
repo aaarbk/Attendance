b582a8956ccac5e8eeb5405c7bfe1c4b06f4e0e8
Akiel K <aaarbk@gmail.com>
2023-02-20 05:51:44 +0300

    fixed days of the week issue

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -36,10 +36,11 @@ class AttendanceController < ApplicationController
     shift_end_time = params[:end_time]
     from = params[:from]
     to = params[:to]
-
+    puts "*************** #{params}"
     employee_ids = params[:employee_ids].map {|e| e.to_i}
+    which_days = params[:day_ids].map{|d| d.to_i}
 
-    shift_ids = Shift.create_shifts(from, to, shift_start_time, shift_end_time)
+    shift_ids = Shift.create_shifts(from, to, shift_start_time, shift_end_time, which_days)
     ShiftAssignment.create_shift_assignments(shift_ids, employee_ids)
     flash[:notice] = "Created shifts!"
   end
